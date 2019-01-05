b3ebb227bddfb2f23d608e7756a4617e1d50e049
Luigino Perez <luigino.mp@gmail.com>
2019-01-05 15:41:37 -0500

    Reworked table bank_user -Removed account ID column -Removed foreign key constraint for accounts
    Reworked table bank_account
    -Removed transaction ID column
    -Added user ID column
    -Added foreign key constraint for user ID
    
    Created table account_transaction
    -Contains number variable for transaction ID
    -Contains timestamp variable for transaction timestamp
    -Contains decimal variable for transaction amount
    -Contains number variable for account ID transaction belongs to
    -Created foreign key constraint for account ID
    
    Reworked stored procedure insert_account
    -Now passes user ID instead of transaction ID
    
    Created trigger insert_transaction
    -Creates new entry in account_transaction table every time there is an update made to a bank_account entry

--- JDBCBank/src/main/resources/JDBCBank_Setup.sql
+++ JDBCBank/src/main/resources/JDBCBank_Setup.sql
@@ -2,23 +2,31 @@
 CREATE TABLE bank_user (
     user_id NUMBER(10) PRIMARY KEY,
     user_name VARCHAR(255) UNIQUE NOT NULL,
-    user_password VARCHAR(255) NOT NULL,
-    account_id NUMBER(10)
+    user_password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE bank_account (
     account_id NUMBER(10) PRIMARY KEY,
     account_balance DECIMAL DEFAULT '0',
-    transaction_id NUMBER(10) DEFAULT '0'
+    user_id NUMBER(10) NOT NULL
 );
 
 CREATE TABLE account_transaction (
-    transaction_date DATE
+    transaction_id NUMBER(10) PRIMARY KEY,
+    transaction_timestamp TIMESTAMP NOT NULL,
+    transaction_amount DECIMAL(10) NOT NULL,
+    account_id NUMBER (10) NOT NULL
 );
 
 ----------------------------------------------------------------CONSTRAINTS
-ALTER TABLE BANK_USER
-    ADD CONSTRAINT user_to_account_foreign_key
+ALTER TABLE bank_account
+    ADD CONSTRAINT account_user_fk
+        FOREIGN KEY (user_id)
+        REFERENCES bank_user (user_id)
+        ON DELETE CASCADE;
+
+ALTER TABLE account_transaction
+    ADD CONSTRAINT transaction_acocunt_fk
         FOREIGN KEY (account_id)
         REFERENCES bank_account (account_id)
         ON DELETE CASCADE;
@@ -35,13 +43,33 @@ CREATE SEQUENCE account_id_sequence
     START WITH 1
     INCREMENT BY 1
     CACHE 20;
+    
+CREATE SEQUENCE transaction_id_sequence
+     MINVALUE 1
+    START WITH 1
+    INCREMENT BY 1
+    CACHE 20;
 
+----------------------------------------------------------------TRIGGERS
+CREATE OR REPLACE TRIGGER insert_transaction
+AFTER UPDATE ON bank_account
+FOR EACH ROW
+DECLARE
+    amount DECIMAL;
+    account_id NUMBER;
+BEGIN
+    amount := :NEW.account_balance - :OLD.account_balance;
+    account_id := :OLD.account_id;
+    INSERT INTO account_transaction
+        VALUES (transaction_id_sequence.NEXTVAL, CURRENT_TIMESTAMP, amount, account_id);
+END;
+/
 ----------------------------------------------------------------STORED PROCEDURES
 CREATE OR REPLACE PROCEDURE insert_user
     (new_name IN VARCHAR2, new_password IN VARCHAR2) AS
 BEGIN
     INSERT INTO bank_user
-        VALUES (user_id_sequence.NEXTVAL, new_name, new_password, NULL);
+        VALUES (user_id_sequence.NEXTVAL, new_name, new_password);
     COMMIT;
 END;
 /
@@ -80,11 +108,10 @@ END;
 /
 
 CREATE OR REPLACE PROCEDURE insert_account
-    (output_id OUT NUMBER) AS
+    (input_id IN NUMBER) AS
 BEGIN
     INSERT INTO bank_account
-        VALUES (account_id_sequence.NEXTVAL,0, NULL);
-    output_id := account_id_sequence.CURRVAL;
+        VALUES (account_id_sequence.NEXTVAL,0, input_id);
     COMMIT;
 END;
 /
@@ -98,21 +125,29 @@ END;
 /
 
 CREATE OR REPLACE PROCEDURE deposit_balance
-    (input_account_id IN NUMBER, input_amount IN DECIMAL) AS
+    (input_id IN NUMBER, input_amount IN DECIMAL) AS
 BEGIN
     UPDATE bank_account
         SET account_balance = account_balance + input_amount
-        WHERE account_id = input_account_id;
+        WHERE account_id = input_id;
 END;
 /
 
 CREATE OR REPLACE PROCEDURE withdraw_balance
-    (input_account_id IN NUMBER, input_amount IN DECIMAL) AS
+    (input_id IN NUMBER, input_amount IN DECIMAL) AS
 BEGIN
     UPDATE bank_account
         SET account_balance = account_balance - input_amount
-        WHERE account_id = input_account_id;
+        WHERE account_id = input_id;
 END;
 /
 
+CREATE OR REPLACE PROCEDURE insert_transaction
+    (input_id IN NUMBER, input_amount IN DECIMAL) AS
+BEGIN
+    INSERT INTO account_transaction
+        VALUES (transaction_id_sequence.NEXTVAL, CURRENT_TIMESTAMP, input_amount, input_id);
+    COMMIT;
+END;
+/   
 commit;
--- JDBCBank/src/main/resources/JDBCBank_Teardown.sql
+++ JDBCBank/src/main/resources/JDBCBank_Teardown.sql
@@ -1,5 +1,7 @@
-drop table bank_user;
-drop table bank_account;
+drop trigger insert_transaction;
 drop table account_transaction;
+drop table bank_account;
+drop table bank_user;
 drop sequence user_id_sequence;
 drop sequence account_id_sequence;
+drop sequence transaction_id_sequence;
